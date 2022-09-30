88b572701696ba287f3d4f0302916b5cbc715f44
Mohammed Zakir Shaikh <109649928+shaikhzakir@users.noreply.github.com>
2022-09-30 14:22:31 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -40,3 +40,20 @@ function myBalance() {
     document.getElementById('tmamount').innerHTML = 0;
   });
 });
+
+
+$(function(){
+    var dtToday = new Date();
+
+    var month = dtToday.getMonth() + 1;
+    var day = dtToday.getDate();
+    var year = dtToday.getFullYear();
+
+    if(month < 10)
+        month = '0' + month.toString();
+    if(day < 10)
+        day = '0' + day.toString();
+
+    var maxDate = year + '-' + month + '-' + day;    
+    $('#txtStart').attr('max', maxDate);
+});
